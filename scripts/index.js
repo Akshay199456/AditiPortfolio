f4c59cbdaf562521bb40c347e54dc464272d9f81
Akshay199456 <akshay.kum94@gmail.com>
2019-12-25 01:07:50 -0600

    Fixed few bugs

--- scripts/index.js
+++ scripts/index.js
@@ -19,12 +19,10 @@ console.log(navbarListener);
 navbarListener.addEventListener("click",function(e){
 	// Activated only if span element is clicked
 	if(e.target !== e.currentTarget){
-		// console.log("Target: " + e.target);
-		// console.log("Current Target: " + e.currentTarget);
 
 		// Add class to current selection
 		e.target.classList.add("spanOnClick");
-		if(previousSpan){
+		if(previousSpan && e.target!==previousSpan){
 			// Removes class from previous selection
 			previousSpan.classList.remove("spanOnClick");
 		}
